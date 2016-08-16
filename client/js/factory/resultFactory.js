ddc5eb70d43b73b4031dcea4386ffa6613cddde7
gmmoraes <gabrielgmmoraes@gmail.com>
2016-08-16 13:11:18 -0300

    token problems solved

--- client/js/factory/resultFactory.js
+++ client/js/factory/resultFactory.js
@@ -2,7 +2,7 @@ myApp.factory('resultFactory', function($http, $compile){
 
     return {
         parse: function() {
-            return $http.get("https://api.github.com/orgs/x-formation/repos?access_token=13ec017dfa16f8c7bbf9273c09128d27265a121d");  // This will return a promise
+            return $http.get("https://api.github.com/orgs/x-formation/repos");  // This will return a promise
         }
         
     };
